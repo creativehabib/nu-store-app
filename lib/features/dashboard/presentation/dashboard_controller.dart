a10e1e73fd51567ba34ef045b372a108a6994a4c
creativehabib <shamolrahaman@gmail.com>
2026-07-19 00:58:58 +0600

    update

--- lib/features/dashboard/presentation/dashboard_controller.dart
+++ lib/features/dashboard/presentation/dashboard_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 import '../../../shared/providers/core_providers.dart';
 import '../data/dashboard_repository.dart';
